66c09af69aad6ba28f23c1c3732409f7e3d4edde
SamudraGanguly <samudraganguly002017@gmail.com>
2026-02-10 13:21:29 +0530

    Done

--- src/components/toppers.tsx
+++ src/components/toppers.tsx
@@ -6,7 +6,7 @@ const Toppers = () => {
       <div className="hero-content text-center">
         <div className="space-y-12">
           <h1 className="text-4xl font-bold">Our Star Achievers</h1>
-          <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 md:gap-12 lg:gap-x-24">
+          <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 md:gap-12 lg:gap-x-24 items-center">
             <TopperCard
               link="/ishani.jpg"
               title="Ishani Mandal"
